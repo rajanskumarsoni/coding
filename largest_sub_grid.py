6bc8bad0cf97556e2b410b90c659f0aed6fcd078
Rajan kumar soni <rajansoni424@gmail.com>
2020-11-04 12:58:58 +0530

    Update largest_sub_grid.py

--- largest_sub_grid.py
+++ largest_sub_grid.py
@@ -16,8 +16,8 @@ def maxsumgrid(dp,k,l):
 def search(grid,mxval):
     l = len(grid)
     dp = [[0 for _ in range(l+1)] for _ in range(l+1)]
-    for i in range(l+1):
-        for j in range(l+1):
+    for i in range(1,l+1):
+        for j in range(1,l+1):
             dp[i][j] = grid[i-1][j-1]-dp[i-1][j-1]+dp[i-1][j]+dp[i][j-1]
     s = 1 
     e = l
